4a0715865a3e505bae0a8e91e4f5d55b6a0959b0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-10 17:18:29 +0300

    add snapshot

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store/index';
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapshot(state);
+  
   return <div>Home</div>;
 };
 

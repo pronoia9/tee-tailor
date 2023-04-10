cf97792bc656d3a2e70e55a72407266019d56b68
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-10 18:37:50 +0300

    fix type, add type else

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
 
-  const generateStyle = (type) => {
-    if ((type = 'filled')) return { backgroundColor: snap.color, color: '#fff' };
-  };
+  const generateStyle = (type) =>
+    type === 'filled' ? { backgroundColor: snap.color, color: getContrastingColor(snap.color), }
+      : type === 'outline' ? { borderWidth: '1px', borderColor: snap.color, color: snap.color, }
+      : {};
 
   return (
-    <button
-      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-      style={generateStyle(type)}
-      tpye={type}
-      onClick={handleClick}>
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)} onClick={handleClick}>
       {title}
     </button>
   );

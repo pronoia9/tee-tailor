5654795c38d02475ed8ddf5b3b7b4af2d9711586
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-10 18:42:00 +0300

    add imports

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import config from '../config/config';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { download } from '../assets';
 
 const Customizer = () => {
   return <div>Customizer</div>;

12038b0ea9861ba05b4141d17e4c17706d1b5a80
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-10 16:22:24 +0300

    add new pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Canvas from '../canvas';
+import Customizer from '../pages/Customizer';
+import Home from '../pages/Home';
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;

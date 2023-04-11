27286d80d51b5414d4736f6ad3f8819788fe3ccc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-12 01:58:35 +0300

    add tab and styles

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -3,10 +3,22 @@ import { useSnapshot } from 'valtio';
 
 import state from '../store';
 
-const Tab = () => {
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
-  
-  return <div>Tab</div>;
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: 'transparent', opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
+      style={activeStyles}
+      onClick={handleClick}>
+      <img className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`} src={tab.icon} alt={tab.name} />
+    </div>
+  );
 };
 
 export default Tab;

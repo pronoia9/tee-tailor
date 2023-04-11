18de837c7ad0eaf12b764422954d84d7f4f5769f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-11 03:25:47 +0300

    add texture and logo

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -19,8 +19,10 @@ const Shirt = () => {
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
-        dispose={null}
-      />
+        dispose={null}>
+        {snap.isFullTexture && <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1} map={fullTexture} />}
+        {snap.isLogoTexture && <Decal position={[0, 0.04, 0.15]} rotation={[0, 0, 0]} scale={0.15} map={logoTexture} />}
+      </mesh>
     </group>
   );
 };

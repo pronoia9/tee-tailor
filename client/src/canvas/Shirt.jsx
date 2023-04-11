1413963d584a91db372585cb93e5ab768660d60d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-11 03:08:49 +0300

    add initial shirt

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -4,8 +4,25 @@ import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
+import state from '../store';
+
 const Shirt = () => {
-  return <div>Shirt</div>;
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb'),
+    logoTexture = useTexture(snap.logoDecal),
+    fullTexture = useTexture(snap.fullDecal);
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      />
+    </group>
+  );
 };
 
 export default Shirt;

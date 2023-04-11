1e66a26694c9408cfd0f5ed91b28b98405ab48b2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-12 02:09:45 +0300

    add imports

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+  
   return <div>ColorPicker</div>;
 };
 

7bd512c3b8c709b6a97ca3f1b31202729cb79c64
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-11 03:02:42 +0300

    add imports

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
 const Shirt = () => {
   return <div>Shirt</div>;

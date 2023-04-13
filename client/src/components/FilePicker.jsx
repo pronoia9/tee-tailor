f5dddbccd7029675d3a34cb03e442a0ad3c697e4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-14 00:55:13 +0300

    add file input

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const FilePicker = () => {
-  return <div>FilePicker</div>;
+import CustomButton from './CustomButton';
+
+const FilePicker = ({ file, setFile, readFile }) => {
+  return (
+    <div className='filepicker-container'>
+      <div className='flex-1 flex-col'>
+        <input id='file-upload' type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+        <label className='filepicker-label' htmlFor='file-upload'>Upload File</label>
+
+        <p className='mt-2 text-gray-500 text-xs truncate'>{file === '' ? 'No file selected' : file.name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default FilePicker;

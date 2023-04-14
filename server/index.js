260588f1c50f92dc5f6606adc81b428a1ad51603
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-14 21:05:52 +0300

    listen on port 8080

--- server/index.js
+++ server/index.js
@@ -11,3 +11,5 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E' });
 });
+
+app.listen(8080, () => console.log('Server has started on port 8080'));
